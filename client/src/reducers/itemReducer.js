8518fc8a6c6f19c177456b0d34020c10fef3bcd0
sergio1124 <mendoza.sergio1124@yahoo.com>
2019-10-24 20:44:09 -0500

    Added store

---
+++ client/src/reducers/itemReducer.js
@@ -0,0 +1,13 @@
+import uuid from "uuid";
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from "../actions/types";
+
+const initialState = {
+  items: [
+    { id: uuid(), name: "Eggs" },
+    { id: uuid(), name: "Milk" },
+    { id: uuid(), name: "Steak" },
+    { id: uuid(), name: "Water" }
+  ]
+};
+
+export default function(state = initialState, action) {}
